b1830324423dce92259a308decc9b2c167a0ec06
Eg-ork-a <eg_ork_a2@mail.ru>
2022-11-28 21:29:55 +0300

    FinishMadeUIForCraft

--- Assets/Scripts/Craft/ArmorsMenu.cs
+++ Assets/Scripts/Craft/ArmorsMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class ArmorsMenu : MonoBehaviour
 {
     public GameObject craftmenu;
+    public GameObject Canvas;
     public bool FlagOpenCraftMenu = false;
 
     public void OpenCraftMenu()
--- Assets/Scripts/Craft/CraftMenu.cs
+++ Assets/Scripts/Craft/CraftMenu.cs
@@ -7,7 +7,12 @@ public class CraftMenu : MonoBehaviour
 {
 
     public GameObject craftmenu;
-    public bool FlagOpenCraftMenu=false;
+    public GameObject toolsmenu;
+    public GameObject weaponsmenu;
+    public GameObject armorsmenu;
+    public GameObject buildingsmenu;
+    public GameObject othersmenu;
+    public bool FlagOpenCraftMenu = false;
     public bool FlagOpenToolsMenu = false;
     public bool FlagOpenWeaponsMenu = false;
     public bool FlagOpenArmorsMenu = false;
@@ -36,6 +41,136 @@ public class CraftMenu : MonoBehaviour
         { 
             craftmenu.SetActive(false);
             FlagOpenCraftMenu=false;
+        }
+    }
+
+    public void OpenToolsMenu()
+    {
+        if (!FlagOpenToolsMenu)
+        {
+            toolsmenu.SetActive(true);
+            weaponsmenu.SetActive(false);
+            armorsmenu.SetActive(false);
+            buildingsmenu.SetActive(false);
+            othersmenu.SetActive(false);
+            FlagOpenToolsMenu = true;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
+        }
+        else
+        {
+            toolsmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
+        }
+    }
+
+    public void OpenWeaponsMenu()
+    {
+        if (!FlagOpenWeaponsMenu)
+        {
+            toolsmenu.SetActive(false);
+            weaponsmenu.SetActive(true);
+            armorsmenu.SetActive(false);
+            buildingsmenu.SetActive(false);
+            othersmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = true;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
+        }
+        else
+        {
+            weaponsmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
+        }
+    }
+
+    public void OpenArmorsMenu()
+    {
+        if (!FlagOpenArmorsMenu)
+        {
+            toolsmenu.SetActive(false);
+            weaponsmenu.SetActive(false);
+            armorsmenu.SetActive(true);
+            buildingsmenu.SetActive(false);
+            othersmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = true;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
+        }
+        else
+        {
+            armorsmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
+        }
+    }
+
+    public void OpenBuildingsMenu()
+    {
+        if (!FlagOpenBuildingsMenu)
+        {
+            toolsmenu.SetActive(false);
+            weaponsmenu.SetActive(false);
+            armorsmenu.SetActive(false);
+            buildingsmenu.SetActive(true);
+            othersmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = true;
+            FlagOpenOthersMenu = false;
+        }
+        else
+        {
+            buildingsmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
+        }
+    }
+
+    public void OpenOthersMenu()
+    {
+        if (!FlagOpenWeaponsMenu)
+        {
+            toolsmenu.SetActive(false);
+            weaponsmenu.SetActive(false);
+            armorsmenu.SetActive(false);
+            buildingsmenu.SetActive(false);
+            othersmenu.SetActive(true);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = true;
+        }
+        else
+        {
+            othersmenu.SetActive(false);
+            FlagOpenToolsMenu = false;
+            FlagOpenWeaponsMenu = false;
+            FlagOpenArmorsMenu = false;
+            FlagOpenBuildingsMenu = false;
+            FlagOpenOthersMenu = false;
         }
     }
 
