fa7e07765bbe7b1a6bcfb8c63e9e42574a2a6bb9
Eg-ork-a <eg_ork_a2@mail.ru>
2022-11-28 19:19:23 +0300

    Made CraftCategories

---
+++ Assets/Scripts/Craft/BuildingsMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildingsMenu : MonoBehaviour
+{
+    public GameObject craftmenu;
+    public bool FlagOpenCraftMenu = false;
+
+    public void OpenCraftMenu()
+    {
+        if (!FlagOpenCraftMenu)
+        {
+            craftmenu.SetActive(true);
+            FlagOpenCraftMenu = true;
+        }
+        else
+        {
+            craftmenu.SetActive(false);
+            FlagOpenCraftMenu = false;
+        }
+    }
+}
