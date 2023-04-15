b499f8b93e5af9e3ecac3a6bff9daee654b765f3
Eg-ork-a <eg_ork_a2@mail.ru>
2023-04-15 11:33:10 +0300

    FixVerstak

--- Assets/Scripts/Craft/ContactVerstak.cs
+++ Assets/Scripts/Craft/ContactVerstak.cs
@@ -1,35 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ContactVerstak : MonoBehaviour
 {
     
+    [FormerlySerializedAs("ButtonAxe")] public GameObject Buttonaxe;
+    [FormerlySerializedAs("ButtonPickAxe")] public GameObject Buttonpickaxe;
+    [FormerlySerializedAs("ButtonSpear")] public GameObject Buttonspear;
+    [FormerlySerializedAs("ButtonAxe2")] public GameObject Buttonaxe2;
+
     // Start is called before the first frame update
-    public GameObject ButtonAxe;
-    public GameObject ButtonPickAxe;
-    public GameObject ButtonSpear;
-    public GameObject ButtonAxe2;
+    private void Start()
+    {
+        Buttonaxe=GameObject.Find("Canvas").transform.Find("CraftMenu/ToolsMenu/ButtonAxe").gameObject;
+        Buttonpickaxe=GameObject.Find("Canvas").transform.Find("CraftMenu/ToolsMenu/ButtonPickAxe").gameObject;
+        Buttonspear=GameObject.Find("Canvas").transform.Find("CraftMenu/WeaponsMenu/ButtonSpear").gameObject;
+        Buttonaxe2=GameObject.Find("Canvas").transform.Find("CraftMenu/WeaponsMenu/ButtonAxe2").gameObject;
+    }
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.name == "player")
         {
-            ButtonAxe.SetActive(true);
-            ButtonPickAxe.SetActive(true);
-            ButtonSpear.SetActive(true);
-            ButtonAxe2.SetActive(true);
+            Buttonaxe.SetActive(true);
+            Buttonpickaxe.SetActive(true);
+            Buttonspear.SetActive(true);
+            Buttonaxe2.SetActive(true);
         }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.name == "player")
         {
-            ButtonAxe.SetActive(false);
-            ButtonPickAxe.SetActive(false);
-            ButtonSpear.SetActive(false);
-            ButtonAxe2.SetActive(false);
+            Buttonaxe.SetActive(false);
+            Buttonpickaxe.SetActive(false);
+            Buttonspear.SetActive(false);
+            Buttonaxe2.SetActive(false);
             
         }
     }
